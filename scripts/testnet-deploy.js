2a9dc24602e0d0683e3a91ea121c219e9bdd3fb1
0xKey <davidlee152025@gmail.com>
2021-08-12 17:43:13 +1000

    fix start block number issue

--- scripts/testnet-deploy.js
+++ scripts/testnet-deploy.js
@@ -7,26 +7,29 @@ async function main() {
   const contracts = getSavedContractAddresses()[hre.network.name];
 
   let currentBlock = await web3.eth.getBlockNumber();
-  currentBlock += 100;
+  currentBlock += 100;  // 20~25 mins
   console.log('startBlock: ' + currentBlock);
   const rewardPerBlock = ethers.utils.parseEther("1.25"); //1.25 token per block
 
   const TokensFarm = await hre.ethers.getContractFactory('TokensFarm');
   console.log();
-  const tokensFarm = await TokensFarm.deploy(contracts["RewardToken"], rewardPerBlock, currentBlock);
+  const tokensFarm = await TokensFarm.deploy(contracts["Hord"], rewardPerBlock, currentBlock, 100, true);
   await tokensFarm.deployed();
   console.log('TokensFarm deployed with address: ', tokensFarm.address);
   saveContractAddress(hre.network.name, 'TokensFarm', tokensFarm.address);
 
-  // await tokensFarm.addPool(contracts["RewardToken"], true);
+  await tokensFarm.addPool(contracts["Hord"], true);
 
-  // let totalRewards = toWeiDenomination(100800)// 10 days approximately
-  // const rewardToken = await hre.ethers.getContractAt("ERC20Mock", contracts["RewardToken"])
-  // await rewardToken.approve(tokensFarm.address, totalRewards);
-  // console.log('Approved rewards token');
+  let totalRewards = ethers.utils.parseEther("100800")// 12 days approximately
+  let tokenArtifiact = await hre.artifacts.readArtifact("ERC20Mock");
+  const rewardToken = await hre.ethers.getContractAt(tokenArtifiact.abi, contracts["Hord"])
+  await rewardToken.approve(tokensFarm.address, totalRewards);
+  console.log('Approved rewards token');
+  console.log('Hord address = ', contracts["Hord"]);
 
-  // console.log('Create new farming pool for reward token');
-  // await tokensFarm.fund(totalRewards);
+  console.log('Create new farming pool for reward token');
+  await tokensFarm.fund(totalRewards);
+  console.log('Farm funded properly.');
 }
 
 // We recommend this pattern to be able to use async/await everywhere
